8acc40e891bd5fdf5502a3fa1c5283d646f0c946
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-09-29 13:05:44 +0530

    Footer

--- Workspace/01-recipes-app/src/components/Footer.js
+++ Workspace/01-recipes-app/src/components/Footer.js
@@ -1,10 +1,11 @@
 import React from "react"
 
+// footer component
 const Footer = () => {
   return (
     <footer className="page-footer">
       <p>
-        &copy; {new Date().getFullYear()} <span>SimplyRecipes</span>. Built with{" "}
+        &copy; {new Date().getFullYear()} <span>SimplyRecipes</span>. Built with
         <a href="https://www.gatsbyjs.com/">Gatsby</a>
       </p>
     </footer>
